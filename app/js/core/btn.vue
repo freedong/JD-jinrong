d744aa2e5450edf959c8852400589893812b2b7a
zxd <yrqc53719@163.com>
2018-06-21 18:13:19 +0800

    first commit

---
+++ app/js/core/btn.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+	<!-- 动态改变class -->
+	<div :class="[btnClass,cname]">
+		<!-- 插槽  从父组件分发内容 -->
+		<slot/>
+	</div>
+</template>
+
+<script>
+	export default{
+		// 父组件向子组件传入数据用props
+		// 子组件向父组件传入数据用事件
+		props:{
+			cname:{
+				type:String,
+				default:"",
+			},
+		},
+		data() {
+			return {
+				btnClass:"btn",
+			}
+		},
+	}
+</script>
+
+<style lang="scss">
+@import "../../css/element.scss";
+.btn{
+	@include btn;
+}
+</style>
---
+++ app/js/core/slider.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+	<section :class="[cname]">
+		<swiper :options="options" :not-next-tick="options.notNextTick">
+			<swiper-slide v-for="item in items" :key="item.href">
+				<router-link :to="{name:item.href}">
+					<img :src="item.src" alt="">
+				</router-link>
+			</swiper-slide>
+		</swiper>
+	</section>
+</template>
+<script>
+	import { swiper, swiperSlide } from "vue-awesome-swiper"
+	export default {
+		components: {
+			swiper,
+			swiperSlide,
+		},
+		props: {
+			 options: {
+			 	type: Object,
+			 	default() {
+			 		return {
+			 			autoplay: true,
+			 			loop:true,
+			 			pagination:{
+			 				el: ".swiper-pagination",
+			 			},
+			 			notNextTick:false,
+			 		}
+			 	},
+			 },
+			 items: {
+			 	type: Array,
+			 	default() {
+			 		// [{href:'',src:''}]
+			 		return []
+			 	}
+			 }
+		},
+	}
+</script>
+<style lang="scss">
+// ~这个符号可以找到路径
+	@import "~swiper/dist/css/swiper.css";
+</style>
